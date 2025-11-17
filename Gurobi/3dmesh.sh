816b894574335484678f52df148989d519c2f3f1
dev2points <24020277@vnu.edu.vn>
2025-11-17 10:31:38 +0000

    rerun double_star and 3dmesh CPLEX_MIP and Gurobi To 150s with new UB

--- Gurobi/3dmesh.sh
+++ Gurobi/3dmesh.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-TO="1800s"
+TO="150s"
 mkdir -p results/$TO/3dmesh
 DATA_DIR="./graph/3dmesh"
 RESULT_DIR="./results/$TO/3dmesh"
 
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_3.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_3.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_168.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_168.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_335.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_335.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_500.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_500.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_3.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_3.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_135.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_135.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_270.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_270.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_400.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_400.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_5.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_5.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_68.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_68.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_137.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_137.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_200.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_200.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_7.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_7.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_35.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_35.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_70.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_70.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_100.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_100.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_8.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_8.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_36.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_36.log
-# ./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_72.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_72.log
-./runlim -r 1800 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_100.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_100.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_3.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_3.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_168.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_168.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_335.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_335.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_2_2_500.txt 2>&1 | tee $RESULT_DIR/3dmesh_2_2_500.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_3.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_3.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_135.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_135.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_270.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_270.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_3_3_400.txt 2>&1 | tee $RESULT_DIR/3dmesh_3_3_400.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_5.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_5.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_68.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_68.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_137.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_137.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_4_4_200.txt 2>&1 | tee $RESULT_DIR/3dmesh_4_4_200.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_7.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_7.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_35.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_35.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_70.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_70.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_5_5_100.txt 2>&1 | tee $RESULT_DIR/3dmesh_5_5_100.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_8.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_8.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_36.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_36.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_72.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_72.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/3dmesh_6_6_100.txt 2>&1 | tee $RESULT_DIR/3dmesh_6_6_100.log
 
--- Gurobi/double_star.sh
+++ Gurobi/double_star.sh
@@ -5,22 +5,22 @@ DATA_DIR="./graph/double_star"
 RESULT_DIR="./results/$TO/double_star"
 
  ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_5.txt 2>&1 | tee $RESULT_DIR/double_star_15_5.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_7.txt 2>&1 | tee $RESULT_DIR/double_star_15_7.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_10.txt 2>&1 | tee $RESULT_DIR/double_star_15_10.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_12.txt 2>&1 | tee $RESULT_DIR/double_star_15_12.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_30_20.txt 2>&1 | tee $RESULT_DIR/double_star_30_20.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_30_25.txt 2>&1 | tee $RESULT_DIR/double_star_30_25.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_35_20.txt 2>&1 | tee $RESULT_DIR/double_star_35_20.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_35_25.txt 2>&1 | tee $RESULT_DIR/double_star_35_25.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_40_20.txt 2>&1 | tee $RESULT_DIR/double_star_40_20.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_40_25.txt 2>&1 | tee $RESULT_DIR/double_star_40_25.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_40_30.txt 2>&1 | tee $RESULT_DIR/double_star_40_30.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_50_20.txt 2>&1 | tee $RESULT_DIR/double_star_50_20.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_50_25.txt 2>&1 | tee $RESULT_DIR/double_star_50_25.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_50_30.txt 2>&1 | tee $RESULT_DIR/double_star_50_30.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_100_20.txt 2>&1 | tee $RESULT_DIR/double_star_100_20.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_100_25.txt 2>&1 | tee $RESULT_DIR/double_star_100_25.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_100_30.txt 2>&1 | tee $RESULT_DIR/double_star_100_30.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_150_20.txt 2>&1 | tee $RESULT_DIR/double_star_150_20.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_150_25.txt 2>&1 | tee $RESULT_DIR/double_star_150_25.log
-# ./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_150_30.txt 2>&1 | tee $RESULT_DIR/double_star_150_30.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_7.txt 2>&1 | tee $RESULT_DIR/double_star_15_7.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_10.txt 2>&1 | tee $RESULT_DIR/double_star_15_10.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_15_12.txt 2>&1 | tee $RESULT_DIR/double_star_15_12.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_30_20.txt 2>&1 | tee $RESULT_DIR/double_star_30_20.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_30_25.txt 2>&1 | tee $RESULT_DIR/double_star_30_25.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_35_20.txt 2>&1 | tee $RESULT_DIR/double_star_35_20.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_35_25.txt 2>&1 | tee $RESULT_DIR/double_star_35_25.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_40_20.txt 2>&1 | tee $RESULT_DIR/double_star_40_20.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_40_25.txt 2>&1 | tee $RESULT_DIR/double_star_40_25.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_40_30.txt 2>&1 | tee $RESULT_DIR/double_star_40_30.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_50_20.txt 2>&1 | tee $RESULT_DIR/double_star_50_20.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_50_25.txt 2>&1 | tee $RESULT_DIR/double_star_50_25.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_50_30.txt 2>&1 | tee $RESULT_DIR/double_star_50_30.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_100_20.txt 2>&1 | tee $RESULT_DIR/double_star_100_20.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_100_25.txt 2>&1 | tee $RESULT_DIR/double_star_100_25.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_100_30.txt 2>&1 | tee $RESULT_DIR/double_star_100_30.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_150_20.txt 2>&1 | tee $RESULT_DIR/double_star_150_20.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_150_25.txt 2>&1 | tee $RESULT_DIR/double_star_150_25.log
+./runlim -r 150 -s 30000 python3 model.py $DATA_DIR/double_star_150_30.txt 2>&1 | tee $RESULT_DIR/double_star_150_30.log
